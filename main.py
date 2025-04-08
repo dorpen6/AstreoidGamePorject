03687611bc945cf8a422778f36cdf907352e1004
dorpen6 <alivalishov20@gmail.com>
2025-04-09 01:46:49 +0200

    loading images to the game, references

---
+++ main.py
@@ -0,0 +1,13 @@
+import pygame
+
+SCREEN = WIDTH = HEIGHT = 800, 800
+
+# Loading Images
+BACKGROUND_IMAGE = pygame.image.load('./starbg.png')
+ALIEN_IMAGE = pygame.image.load('./alienShip.png')
+PLAYER_SPACE_ROCKET_IMAGE = pygame.image.load('./spaceRocket.png')
+STAR_IMAGE = pygame.image.load('./star.png')
+ASTEROID50 = pygame.image.load('./asteroid50.png')
+ASTEROID100 = pygame.image.load('./asteroid100.png')
+ASTEROID150 = pygame.image.load('./asteroid150.png')
+
